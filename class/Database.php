1038d0e140c14ffc7880b5d44846589e503cfbde
Gulam Hussain <hussainkhan1200@gmail.com>
2017-12-10 22:31:55 +0530

    Database class, and query method added

---
+++ class/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+* This Database class is used for interacting with database
+*/
+class Database
+{
+    //Database Connection using PDO
+    protected function connect()
+    {
+        $host='localhost';
+        $db='admin-dashboard';
+        try
+        {
+            $dbConnection=new PDO("mysql:host=$host;dbname=$db",'root','');
+            $dbConnection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            return $dbConnection;
+        }
+        catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+
+    /*Executing Select prepare statment
+     This function recieve two parameters
+     one is the array of data to be used and another one is prepared statement
+    */
+
+    protected function select($data,$query)
+    {
+        if($query->execute($data)) //executing query, if success then return the object itself;
+        {
+            return $query;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+?>
