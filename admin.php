b7eafefa479dcdfae939aa8a771fbb4fcb871e46
Gulam Hussain <hussainkhan1200@gmail.com>
2017-12-11 23:04:57 +0530

    Created a Bootstrap navbar

---
+++ admin.php
@@ -0,0 +1,21 @@
+<?php
+$title="Admin Dashboard";
+include_once 'include.php';
+?>
+<div class="fluid-container">
+    <div class="col-xs-12 col-sm-8 col-md-12">
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#">Admin Dashboard</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Page 1</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+          </div>
+        </nav>
+    </div>
+</div>
